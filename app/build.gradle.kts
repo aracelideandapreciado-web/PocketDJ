6ed15a3086e9ec215bfc486588ae97012dd6f743
aracelideandapreciado-web <aracelideandapreciado@gmail.com>
2026-09-19 16:22:57 -0600

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.pocketdj"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.pocketdj"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+
+    implementation("androidx.media3:media3-exoplayer:1.9.1")
+}
