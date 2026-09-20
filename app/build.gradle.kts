ab34a35673a8d80bef68b16dd66a176e184ffa41
aracelideandapreciado-web <aracelideandapreciado@gmail.com>
2026-09-19 18:04:09 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,47 +1,39 @@
 plugins {
-id("com.android.application")
-id("org.jetbrains.kotlin.android")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-namespace = "com.pocketdj"
-compileSdk = 35
+    namespace = "com.pocketdj"
+    compileSdk = 36
 
-defaultConfig {
-    applicationId = "com.pocketdj"
-    minSdk = 24
-    targetSdk = 35
+    defaultConfig {
+        applicationId = "com.pocketdj"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+    }
 
-    versionCode = 1
-    versionName = "1.0"
-}
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
 
-buildTypes {
-    release {
-        isMinifyEnabled = false
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-kotlinOptions {
-    jvmTarget = "17"
-}
-
-
-}
-
 dependencies {
-implementation("androidx.core:core-ktx:1.17.0")
-implementation("androidx.appcompat:appcompat:1.7.1")
-implementation("androidx.activity:activity-ktx:1.10.1")
-
-implementation("androidx.media3:media3-exoplayer:1.9.1")
-
-implementation("dev.ffmpegkit-maintained:ffmpeg-kit-full:8.1.7")
-
-
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.media3:media3-exoplayer:1.9.1")
 }
